1b09bd8be7f1856a611793a4f7d6d43926d93302
rleed <101502594+rleed@users.noreply.github.com>
2024-06-12 08:27:27 -0400

    Add the official Public Relations Office npub to the team

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
         <hr className="border-gray-400 border-t w-full flex-grow"/>
       </div>
       <section className="flex xl:flex-row flex-col items-start justify-between">
+        <NostrProfile name="official" title="PRO" pubkey="d7d8109ee43657ce6056ada4653006bbb641f31e50e85243681c2724507811ec"/>
         <NostrProfile name="Simon" title="CEO" pubkey="db625e7637543ca7d7be65025834db318a0c7b75b0e23d4fb9e39229f5ba6fa7"/>
         <NostrProfile name="Arkinox" title="CTO" pubkey="e8ed3798c6ffebffa08501ac39e271662bfd160f688f94c45d692d8767dd345a"/>
         <NostrProfile name="Wemerson" title="Developer" pubkey="56d57bf11aed78a989a7f042a786e1f09c83b1e8360b0462cbf1377454657d1c"/>
